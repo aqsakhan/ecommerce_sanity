23e96be8a96528917553eac377113d58718a8bad
aqsa khan <aqsa250bpl@outlook.com>
2022-05-10 18:29:22 +0530

    first-commit

---
+++ sanity/schemas/product.js
@@ -0,0 +1,41 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'array',
+            of: [{ type: 'image' }],
+            options: {
+                hotspot: 'true'     // this property makes it easier to position the images
+            }
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            // for ulr's(or for unique strings) we will have the type set as slug as provided by sanity
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 90,
+            }
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number',
+        },
+        {
+            name: 'details',
+            title: 'Details',
+            type: 'string',
+        }
+    ]
+}
